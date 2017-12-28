3b44210500c8d3058abdde1f25b61b6e74ba9f70
Michael A Nowiszewski <micski@gmail.com>
2017-12-28 22:38:56 +0000

    v2

---
+++ js.js
@@ -0,0 +1,86 @@
+/* makeTurkeytSandwich
+    Get one slice of bread.
+    Add turkey.
+    Put a slice of bread on top. */
+var ham
+
+function makeTurkeySandwich(filling){
+    
+    
+}
+
+
+makeSandwichWith(ham)
+
+
+console.log('My Todos', todos)
+
+
+var todos = ["item 1", "item 2", "item 3"];
+
+undefined
+function functionName() {}
+undefined
+function displayTodos() { 
+	console.log('My todos:', todos);
+}
+undefined
+function displayTodos() { 
+	console.log('My todos:', todos);
+}
+undefined
+todos
+(3) ["item 1", "item 2", "item 3"]
+function displayTodos() { 
+	console.log('My todos:', todos);
+}
+undefined
+function displayTodos() { 
+	console.log('My todos:', todos);
+}
+undefined
+displayTodos ()
+VM1866:2 My todos: (3) ["item 1", "item 2", "item 3"]
+undefined
+
+
+
+function addTodo() {
+	todos.push('new todo')
+}
+undefined
+function addTodo() {
+	todos.push('new todo');
+}
+undefined
+addTodo()
+undefined
+todos
+(4) ["item 1", "item 2", "item 3", "new todo"]
+addTodo()
+undefined
+displayTodos()
+VM1866:2 My todos: (5) ["item 1", "item 2", "item 3", "new todo", "new todo"]
+undefined
+
+
+function changeTodo(position, newValue){
+    todos[position] = newValue;
+	displayTodos();
+}
+undefined
+displayTodos
+ƒ displayTodos() { 
+	console.log('My todos:', todos);
+}
+changeTodo()
+VM1866:2 My todos: (6) ["changed", "item 2", "item 3", "new todo", "new todo", "new todo", undefined: undefined]
+undefined
+function changeTodo(position, newValue){
+    todos[position] = newValue;
+	displayTodos();
+}
+undefined
+changeTodo()
+VM1866:2 My todos: (6) ["changed", "item 2", "item 3", "new todo", "new todo", "new todo", undefined: undefined]
+undefined
