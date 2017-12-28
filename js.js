7dca09d0c5c047a9ea6a387dac6b297a6ac2b656
Michael A Nowiszewski <micski@gmail.com>
2017-12-28 22:43:12 +0000

    ud2

--- js.js
+++ js.js
@@ -84,3 +84,74 @@ undefined
 changeTodo()
 VM1866:2 My todos: (6) ["changed", "item 2", "item 3", "new todo", "new todo", "new todo", undefined: undefined]
 undefined
+
+
+
+todos
+(6) ["item 1", "item 2", "item 3", "new todo", "new todo", "new todo"]
+function changeTodo(position, newValue){
+    todos[position] = newValue;
+}
+undefined
+undefined
+function changeTodo(position, newValue){
+    todos[position] = newValue;
+}
+undefined
+changeTodo(0, 'changed')
+undefined
+displayTodos()
+VM1866:2 My todos: (6) ["changed", "item 2", "item 3", "new todo", "new todo", "new todo"]
+undefined
+todos[position] = newValue;
+disp
+VM2661:1 Uncaught ReferenceError: position is not defined
+    at <anonymous>:1:7
+(anonymous) @ VM2661:1
+function changeTodo(position, newValue){
+    todos[position] = newValue;
+	displayTodos();
+}
+undefined
+displayTodos
+ƒ displayTodos() { 
+	console.log('My todos:', todos);
+}
+changeTodo()
+VM1866:2 My todos: (6) ["changed", "item 2", "item 3", "new todo", "new todo", "new todo", undefined: undefined]
+undefined
+function changeTodo(position, newValue){
+    todos[position] = newValue;
+	displayTodos();
+}
+undefined
+changeTodo()
+VM1866:2 My todos: (6) ["changed", "item 2", "item 3", "new todo", "new todo", "new todo", undefined: undefined]
+undefined
+function deleteTodo(position) {
+todos.splice (position, 1);
+displayTodos();
+}
+undefined
+function deleteTodos(position) {
+todos.splice (position, 1);
+displayTodos();
+}
+undefined
+function deleteTodo(position) {
+todos.splice (position, 1);
+displayTodos();
+}
+undefined
+deleteTodo(0)
+VM1866:2 My todos: (5) ["item 2", "item 3", "new todo", "new todo", "new todo", undefined: undefined]
+undefined
+deleteTodo(2)
+VM1866:2 My todos: (4) ["item 2", "item 3", "new todo", "new todo", undefined: undefined]
+undefined
+deleteTodo(5)
+VM1866:2 My todos: (4) ["item 2", "item 3", "new todo", "new todo", undefined: undefined]
+undefined
+deleteTodo(4)
+VM1866:2 My todos: (4) ["item 2", "item 3", "new todo", "new todo", undefined: undefined]
+undefined
